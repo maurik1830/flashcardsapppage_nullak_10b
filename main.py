a2454572f1746d21aa94abc88a384d4568f9745b
maurik1830 <nagyemoric@gmail.com>
2023-06-01 23:14:19 +0200

    flashcardsapppage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 import random
-anime = ["Kodomo", "Sónen", "Seinen", "Sódzsó", "Dzsoszei", "Ecchi", "Hentai", "Shónen-ai", "Jaoi", "Sódzsó-ai", "Juri"]
+anime = ["Kodomo", "Sónen", "Szeinen", "Sódzsó", "Dzsoszei", "Ecchi", "Hentai", "Shónen-ai", "Jaoi", "Sódzsó-ai", "Juri"]
 
 with open("term_def.json", "r", encoding="utf-8") as forras:
     adatok = json.load(forras)
@@ -8,9 +8,9 @@ with open("term_def.json", "r", encoding="utf-8") as forras:
 randsz = random.randint(0, len(anime))
 adat_ani = anime[randsz]
 
-adat_ani2 = adatok["anime"][0]["Kodomo"]
+adat_ani2 = adatok["anime"][0][adat_ani]
 
-print(adatok["anime"][0]["Kodomo"])
+
 
 
 
